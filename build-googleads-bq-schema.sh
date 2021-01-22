a65a26caddb0921affcd2aeb8b0452e4c51cd515
Vardan Manucharyan <sd003gm@gmail.com>
2021-01-22 12:45:43 +0300

    build-googleads-bq-schema.sh - fix absolute path

--- build-googleads-bq-schema.sh
+++ build-googleads-bq-schema.sh
@@ -3,5 +3,5 @@ OUT_DIR="./lib"
 protoc \
   --bq-schema_out=./gen/bq-schema \
   --proto_path=. \
-  --proto_path=/Users/vardan/go/src/github.com/GoogleCloudPlatform/protoc-gen-bq-schema \
+  --proto_path=$GOPATH/src/github.com/GoogleCloudPlatform/protoc-gen-bq-schema \
   ./google/ads/googleads/v6/**/*.proto
